c50a0faf13c114fed53eaf51291a26d7f91491f1
rjsdn031 <rjsdn031@naver.com>
2021-09-26 23:43:48 +0900

    Modify and Optimize code to follow the single responsibillity principle

--- pyBubbleSort.py
+++ pyBubbleSort.py
@@ -1,11 +1,11 @@
-def bubblesort(data:list, n:int):
+def bubblesort(data:list):
+    n = len(data)
     for j in range(0,n-1):
         for i in range(1, n-j):
             if data[i-1] > data[i]:
                 temp = data[i-1]
                 data[i-1] = data[i]
                 data[i] = temp
-                print(data)
     return
 
 data1 = [9,2,7,3,5,6,0,1,4,8]   ##random
@@ -13,10 +13,12 @@ data2 = [9,8,7,6,5,4,3,2,1,0]   ##reverse
 
 print("data1 : ", end = "")
 print(data1)
-bubblesort(data1, len(data1))
+print("data1-BubbleSort : ", end = "")
+bubblesort(data1)
 print(data1)
 
 print("data2 : ", end = "")
 print(data2)
-bubblesort(data2, len(data2))
+print("data2-BubbleSort : ", end = "")
+bubblesort(data2)
 print(data2)
