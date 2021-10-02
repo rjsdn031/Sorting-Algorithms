b479bf6d6e522bdb9f99a4380a77f27115c767cc
rjsdn031 <rjsdn031@naver.com>
2021-10-02 22:26:39 +0900

    heapsort -firstcommit

---
+++ pyHeapSort.py
@@ -0,0 +1,19 @@
+def heapsort(data:list):
+
+    return
+
+
+data1 = [9,2,7,3,5,6,0,1,4,8]   ##random
+data2 = [9,8,7,6,5,4,3,2,1,0]   ##reverse
+
+print("data1 : ", end = "")
+print(data1)
+print("data1-BubbleSort : ", end = "")
+heapsort(data1)
+print(data1)
+
+print("data2 : ", end = "")
+print(data2)
+print("data2-BubbleSort : ", end = "")
+heapsort(data2)
+print(data2)
